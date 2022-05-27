d155e39ab75b427197d9fe83510b37ae03dd46c4
masanara <masanara@gmail.com>
2022-05-27 06:44:53 +0000

    update message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from the server! from codespace</h1>\n');
+  res.send('<h1>Hello from the server!</h1>\n');
 });
 
 app.get('/health', (req, res) => {
